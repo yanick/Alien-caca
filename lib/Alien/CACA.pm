a3169fd73b81c69c36fde0c3761168cf677df847
Yanick Champoux <yanick@cpan.org>
2018-07-27 19:21:40 -0400

    original commit

--- lib/Alien/CACA.pm
+++ lib/Alien/CACA.pm
@@ -1,13 +1,47 @@
-package Alien::CACA;
-# ABSTRACT: 
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-=cut
+package Alien::caca;
+# ABSTRACT: Alien package for the Colored ASCII Art library
 
 use strict;
 use warnings;
 
 1;
+
+
+=head1 SYNOPSIS 
+
+In your Build.PL:
+
+    use Module::Build;
+    use Alien::caca;
+    my $builder = Module::Build->new(
+        ...
+        configure_requires => {
+            'Alien::caca' => '0',
+            ...
+        },
+        extra_compiler_flags => Alien::caca->cflags,
+        extra_linker_flags   => Alien::caca->libs,
+        ...
+    );
+ 
+    $build->create_build_script;
+
+In your Makefile.PL:
+
+    use ExtUtils::MakeMaker;
+    use Config;
+    use Alien::caca;
+    
+    WriteMakefile(
+    ...
+    CONFIGURE_REQUIRES => {
+        'Alien::caca' => '0',
+    },
+    CCFLAGS => Alien::caca->cflags . " $Config{ccflags}",
+    LIBS    => [ Alien::caca->libs ],
+    ...
+    );
+
+=head1 DESCRIPTION 
+
+This distribution installs L<libcaca|http://caca.zoy.org/wiki/libcaca> so that it can be used by other Perl distributions.  Mostly L<Term::Caca>.
